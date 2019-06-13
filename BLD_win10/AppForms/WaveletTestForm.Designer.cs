3dc4f70e3be2c6883ba4ab411220f98e452ba84b
春晖 李 <21321278@qq.com>
2019-06-13 08:35:22 +0800

    +小波测试

---
+++ BLD_win10/AppForms/WaveletTestForm.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace BLD_win10.AppForms
+{
+    partial class WaveletTestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // WaveletTestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1064, 733);
+            this.Name = "WaveletTestForm";
+            this.Text = "WaveletTestForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ BLD_win10/MainForm.cs
@@ -0,0 +1,40 @@
+﻿using BLD_win10.ApplicationForm;
+using System;
+using System.Windows.Forms;
+
+namespace BLD_win10
+{
+    public partial class MainForm : Form
+    {
+        HistogramForm histogramForm = new HistogramForm();
+        AC6623TestForm AC6623TestForm = new AC6623TestForm();
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            histogramForm.MdiParent = this;
+            histogramForm.Show();
+            //histogramForm.WindowState = FormWindowState.Maximized;
+        }
+
+        private void aC6623测试ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AC6623TestForm.MdiParent = this;
+            AC6623TestForm.Show();
+            //AC6623TestForm.WindowState = FormWindowState.Maximized;
+        }
+
+        private void WaveletTestToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
