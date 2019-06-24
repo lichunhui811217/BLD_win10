664aa20ed626609cb1f5ed3d92c361ebb4b595b7
春晖 李 <21321278@qq.com>
2019-06-24 09:13:15 +0800

    11

---
+++ BLD_win10/AppForms/HistoricalGraphForm.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace BLD_win10.AppForms
+{
+    partial class HistoricalGraphForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HistoricalGraphForm));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.LegendItem legendItem1 = new System.Windows.Forms.DataVisualization.Charting.LegendItem();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.LegendItem legendItem2 = new System.Windows.Forms.DataVisualization.Charting.LegendItem();
+            System.Windows.Forms.DataVisualization.Charting.LegendItem legendItem3 = new System.Windows.Forms.DataVisualization.Charting.LegendItem();
+            System.Windows.Forms.DataVisualization.Charting.LegendItem legendItem4 = new System.Windows.Forms.DataVisualization.Charting.LegendItem();
+            System.Windows.Forms.DataVisualization.Charting.LegendItem legendItem5 = new System.Windows.Forms.DataVisualization.Charting.LegendItem();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.histogramChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.histogramChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            resources.ApplyResources(this.splitContainer1, "splitContainer1");
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            resources.ApplyResources(this.splitContainer1.Panel1, "splitContainer1.Panel1");
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.histogramChart);
+            resources.ApplyResources(this.splitContainer1.Panel2, "splitContainer1.Panel2");
+            this.splitContainer1.TabStop = false;
+            // 
+            // treeView1
+            // 
+            this.treeView1.CheckBoxes = true;
+            resources.ApplyResources(this.treeView1, "treeView1");
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            ((System.Windows.Forms.TreeNode)(resources.GetObject("treeView1.Nodes"))),
+            ((System.Windows.Forms.TreeNode)(resources.GetObject("treeView1.Nodes1"))),
+            ((System.Windows.Forms.TreeNode)(resources.GetObject("treeView1.Nodes2")))});
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.TreeView1_AfterSelect);
+            // 
+            // histogramChart
+            // 
+            this.histogramChart.BorderlineColor = System.Drawing.Color.Black;
+            this.histogramChart.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
+            chartArea1.AxisX.MajorGrid.Enabled = false;
+            chartArea1.AxisX.MajorTickMark.Enabled = false;
+            chartArea1.Name = "ChartArea1";
+            chartArea2.AxisX.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            chartArea2.Name = "ChartArea2";
+            this.histogramChart.ChartAreas.Add(chartArea1);
+            this.histogramChart.ChartAreas.Add(chartArea2);
+            resources.ApplyResources(this.histogramChart, "histogramChart");
+            legendItem1.Name = "LegendItem1";
+            legend1.CustomItems.Add(legendItem1);
+            legend1.Enabled = false;
+            legend1.IsDockedInsideChartArea = false;
+            legend1.Name = "Legend1";
+            legendItem2.Color = System.Drawing.Color.Red;
+            legendItem2.Name = "报警";
+            legendItem3.Color = System.Drawing.Color.Lime;
+            legendItem3.Name = "正常";
+            legendItem4.Color = System.Drawing.Color.Orange;
+            legendItem4.Name = "超上限";
+            legendItem5.Color = System.Drawing.Color.DodgerBlue;
+            legendItem5.Name = "吹灰";
+            legend2.CustomItems.Add(legendItem2);
+            legend2.CustomItems.Add(legendItem3);
+            legend2.CustomItems.Add(legendItem4);
+            legend2.CustomItems.Add(legendItem5);
+            legend2.IsDockedInsideChartArea = false;
+            legend2.Name = "Legend2";
+            this.histogramChart.Legends.Add(legend1);
+            this.histogramChart.Legends.Add(legend2);
+            this.histogramChart.Name = "histogramChart";
+            this.histogramChart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            this.histogramChart.Series.Add(series1);
+            this.histogramChart.Series.Add(series2);
+            title1.DockedToChartArea = "ChartArea1";
+            title1.IsDockedInsideChartArea = false;
+            title1.Name = "Title1";
+            title1.Text = "图表1";
+            title2.DockedToChartArea = "ChartArea2";
+            title2.IsDockedInsideChartArea = false;
+            title2.Name = "Title2";
+            title2.Text = "棒图2";
+            this.histogramChart.Titles.Add(title1);
+            this.histogramChart.Titles.Add(title2);
+            this.histogramChart.Click += new System.EventHandler(this.histogramChart_Click);
+            // 
+            // HistoricalGraphForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "HistoricalGraphForm";
+            this.Load += new System.EventHandler(this.HistogramForm_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.histogramChart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart histogramChart;
+    }
+}
---
+++ BLD_win10/AppForms/HistoricalGraphForm.cs
@@ -0,0 +1,94 @@
+﻿using BLD_win10.Device;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace BLD_win10.AppForms
+{
+    public partial class HistoricalGraphForm : Form
+    {
+        public HistoricalGraphForm()
+        {
+            InitializeComponent();
+        }
+
+        Random random = new Random();
+
+        private void HistogramForm_Load(object sender, EventArgs e)
+        {
+            histogramChart.Titles.Clear();
+            histogramChart.Series.Clear();
+            histogramChart.ChartAreas.Clear();
+
+            foreach (Boiler aBoiler in DevicesDataCenter.boilersList)
+            {
+                int aBoilerID = aBoiler.BoilerID;
+                string aBoilerCaption = aBoiler.Caption;
+
+                string chartArea_Name = "ChartArea_" + aBoilerID.ToString();
+                ChartArea chartArea = new ChartArea(chartArea_Name);
+                chartArea.AxisX.MajorGrid.Enabled = false;
+                chartArea.AxisX.MajorTickMark.Enabled = false;
+                //chartArea.AxisX.Interval = 1;
+                //chartArea.AxisX.IntervalOffset = 1;
+                chartArea.AxisX.IntervalAutoMode = IntervalAutoMode.VariableCount;
+
+                histogramChart.ChartAreas.Add(chartArea);
+
+                Title title = new Title();
+                title.Name = "Title_" + aBoilerID.ToString();
+                title.DockedToChartArea = chartArea_Name;
+                title.IsDockedInsideChartArea = false;
+                title.Text = aBoilerCaption;
+                histogramChart.Titles.Add(title);
+
+                Series series = new Series();
+                series.Name= "Series_" + aBoilerID.ToString();
+                series.ChartType = SeriesChartType.Spline;
+                series.ChartArea = chartArea_Name;
+                series.Legend = histogramChart.Legends.First().Name;
+                histogramChart.Series.Add(series);
+            }
+        }
+
+        private void histogramChart_Click(object sender, EventArgs e)
+        {
+            foreach (Series aSeries in histogramChart.Series)
+            {
+                aSeries.Points.Clear();
+            }
+
+            foreach (Sensor aSensor in DevicesDataCenter.allSensorsList)
+            {
+                //数据点的填充
+                double tempData = random.NextDouble() * 10.0;  //取随机数*10 填充Y值 TODO:
+                Series series = histogramChart.Series.FindByName("Series_" + aSensor.BoilerID.ToString());
+                series.Points.Add(aSensor);
+                aSensor.SetYValue(tempData);
+
+                ChartArea chartArea = histogramChart.ChartAreas.FindByName("ChartArea_" + aSensor.BoilerID.ToString());//按名称填充ChartAreas.Series
+
+                //按点生成X轴
+                CustomLabel customLabel = new CustomLabel
+                {
+                    FromPosition = aSensor.XValue - 0.5,
+                    ToPosition = aSensor.XValue + 0.5,
+                    Text = aSensor.XValue.ToString()
+                };
+                chartArea.AxisX.CustomLabels.Add(customLabel);
+            }
+        }
+
+        private void TreeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            //TODO: edit Series
+        }
+    }
+}
